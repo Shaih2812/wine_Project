7a8ba6f0caf1b68da66fe61b853c6b61e244235c
OmerSheich99 <omer.sheich@gmail.com>
2025-01-05 12:08:13 +0200

    Changed FilterOptions class and changed "Alcohol content" to "Alcohol Levels"

--- src/FilterOptions.java
+++ src/FilterOptions.java
@@ -5,12 +5,14 @@ public class FilterOptions {
     private final QueryBuilder queryBuilder;
     private final QueryExecutor queryExecutor;
     private final NumberOfWines numberOfWines;
+    private final QualityFilter qualityFilter;
 
     public FilterOptions(Scanner scanner, QueryBuilder queryBuilder, QueryExecutor queryExecutor) {
         this.scanner = scanner;
         this.queryBuilder = queryBuilder;
         this.queryExecutor = queryExecutor;
         this.numberOfWines = new NumberOfWines(scanner, queryExecutor);
+        this.qualityFilter = new QualityFilter(scanner, queryBuilder);
     }
 
     public void showFilterOptions() {
@@ -18,7 +20,7 @@ public class FilterOptions {
             System.out.println("Filter by:");
             System.out.println("1. Color");
             System.out.println("2. Quality");
-            System.out.println("3. Alcohol Content");
+            System.out.println("3. Alcohol Levels");
             System.out.println("4. Apply Filters");
             System.out.println("5. Reset Filters");
             System.out.println("6. Back to Main Menu");
@@ -30,7 +32,7 @@ public class FilterOptions {
                     filterByColor();
                     break;
                 case 2:
-                    filterByQuality();
+                    qualityFilter.apply(); // Delegate to QualityFilter
                     break;
                 case 3:
                     filterByAlcoholContent();
@@ -55,23 +57,9 @@ public class FilterOptions {
         queryBuilder.addCondition("color = '" + color + "'");
     }
 
-    private void filterByQuality() {
-        System.out.println("Quality options:");
-        System.out.println("1. Extremely dissatisfied");
-        System.out.println("2. Moderately dissatisfied");
-        System.out.println("3. Slightly dissatisfied");
-        System.out.println("4. Neutral");
-        System.out.println("5. Slightly satisfied");
-        System.out.println("6. Moderately satisfied");
-        System.out.println("7. Extremely satisfied");
-        System.out.println("Enter quality to filter by number: ");
-        String quality = scanner.next();
-        queryBuilder.addCondition("quality = '" + quality + "'");
-    }
-
     private void filterByAlcoholContent() {
-        float minAlcohol = InputHandler.getFloatInput(scanner, "Enter minimum alcohol content: ");
-        float maxAlcohol = InputHandler.getFloatInput(scanner, "Enter maximum alcohol content: ");
+        float minAlcohol = InputHandler.getFloatInput(scanner, "Enter minimum alcohol levels: ");
+        float maxAlcohol = InputHandler.getFloatInput(scanner, "Enter maximum alcohol levels: ");
         queryBuilder.addCondition("alcohol BETWEEN " + minAlcohol + " AND " + maxAlcohol);
     }
 
